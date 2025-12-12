703c5becdce6046d75e4f3150f2e6caef53ff806
Bina Halder <105237372+BinaHalder@users.noreply.github.com>
2025-12-12 23:11:11 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+model = joblib.load("crop_model.pkl")
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    features = ['N','P','K','temperature','humidity','ph','rainfall']
+    x = np.array([data[f] for f in features]).reshape(1,-1)
+    pred = model.predict(x)[0]
+    label = le.inverse_transform([pred])[0]
+    return jsonify({"recommended_crop": label})
+
+if __name__ == '__main__':
+    app.run()
