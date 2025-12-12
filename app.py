46b117dee93ca107a205a94a3f374f434278b2d5
Bina Halder <105237372+BinaHalder@users.noreply.github.com>
2025-12-13 02:19:49 +0600

    app.py

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
-
 from flask_cors import CORS
+
+# Initialize app
+app = Flask(__name__)
 CORS(app)
 
-app = Flask(__name__)
-
+# Load model and label encoder
 model = joblib.load("crop_model.pkl")
 
 
+# Home route
+@app.route("/")
+def home():
+    return "Crop Recommendation API is running"
+
+# Prediction route
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    features = ['N','P','K','temperature','humidity','ph','rainfall']
-    x = np.array([data[f] for f in features]).reshape(1,-1)
-    pred = model.predict(x)[0]
-    label = le.inverse_transform([pred])[0]
-    return jsonify({"recommended_crop": label})
+    features = ['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall']
+    x = np.array([data[f] for f in features]).reshape(1, -1)
 
-if __name__ == '__main__':
-    app.run()
+    pred = model.predict(x)[0]
+ 
+
+    return jsonify({"crop": label})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
+
 
